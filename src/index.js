4b5c078472e93aa32b75d2583a5df35a9689f584
Luiz Torres <torresluizgtech@gmail.com>
2021-11-07 16:18:53 -0300

    feat: error handling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
+require('express-async-errors');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
